a47dfdb489ff6169283ba318031cf7dd18b82355
Thomas Hourdel <thomashourdel@unity3d.com>
2016-02-19 16:48:06 +0100

    [lens] Added advanced vignette mode & lens distortion effect

--- UnityProject/Assets/Standard Assets/Effects/CinematicEffects(BETA)/LensAberrations/Editor/LensAberrationsEditor.cs
+++ UnityProject/Assets/Standard Assets/Effects/CinematicEffects(BETA)/LensAberrations/Editor/LensAberrationsEditor.cs
@@ -10,6 +10,7 @@ namespace UnityStandardAssets.CinematicEffects
     public class LensAberrationsEditor : Editor
     {
         private Dictionary<FieldInfo, List<SerializedProperty>> m_GroupFields = new Dictionary<FieldInfo, List<SerializedProperty>>();
+        private List<SerializedProperty> m_SimpleProperties = new List<SerializedProperty>(); 
         private List<SerializedProperty> m_AdvancedProperties = new List<SerializedProperty>();
 
         private LensAberrations concreteTarget
@@ -33,7 +34,10 @@ namespace UnityStandardAssets.CinematicEffects
                 if (property != null)
                 {
                     settingsGroup.Add(property);
-                    if (setting.GetCustomAttributes(typeof(LensAberrations.AdvancedSetting), false).Length > 0)
+
+                    if (setting.GetCustomAttributes(typeof(LensAberrations.SimpleSetting), false).Length > 0)
+                        m_SimpleProperties.Add(property);
+                    else if (setting.GetCustomAttributes(typeof(LensAberrations.AdvancedSetting), false).Length > 0)
                         m_AdvancedProperties.Add(property);
                 }
             }
@@ -67,8 +71,17 @@ namespace UnityStandardAssets.CinematicEffects
                         GUILayout.Space(3);
                         foreach (var field in group.Value.Where(x => x.propertyPath != group.Key.Name + ".enabled"))
                         {
-                            if (m_AdvancedProperties.Contains(field) && concreteTarget.chromaticAberration.mode != LensAberrations.ChromaticAberrationMode.Advanced)
-                                continue;
+                            if (group.Key.FieldType == typeof(LensAberrations.VignetteSettings))
+                            {
+                                if (m_SimpleProperties.Contains(field) && concreteTarget.vignette.mode != LensAberrations.SettingsMode.Simple ||
+                                    m_AdvancedProperties.Contains(field) && concreteTarget.vignette.mode != LensAberrations.SettingsMode.Advanced)
+                                    continue;
+                            }
+                            else
+                            {
+                                if (m_AdvancedProperties.Contains(field) && concreteTarget.chromaticAberration.mode != LensAberrations.SettingsMode.Advanced)
+                                    continue;
+                            }
 
                             EditorGUILayout.PropertyField(field);
                         }

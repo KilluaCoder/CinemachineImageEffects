a47ddd39f9fa5d477d79840ba326530f445403a4
Keijiro Takahashi <keijiro@unity3d.com>
2016-04-22 18:16:34 +0900

    [SSAO] Added a warning message for the G-buffer source mode

--- UnityProject/Assets/Standard Assets/Effects/CinematicEffects(BETA)/AmbientOcclusion/Editor/AmbientOcclusionEditor.cs
+++ UnityProject/Assets/Standard Assets/Effects/CinematicEffects(BETA)/AmbientOcclusion/Editor/AmbientOcclusionEditor.cs
@@ -26,6 +26,10 @@ namespace UnityStandardAssets.CinematicEffects
             "The ambient-only mode is currently disabled; " +
             "it requires G-buffer source and HDR rendering.";
 
+        static string _textGBufferNote =
+            "Forward opaque objects don't go in the G-buffer. " +
+            "This may lead to artifacts.";
+
         void OnEnable()
         {
             _intensity = serializedObject.FindProperty("settings.intensity");
@@ -60,10 +64,12 @@ namespace UnityStandardAssets.CinematicEffects
             EditorGUILayout.PropertyField(_occlusionSource);
 
             if (!_occlusionSource.hasMultipleDifferentValues &&
-                _occlusionSource.enumValueIndex == (int)AmbientOcclusion.OcclusionSource.GBuffer &&
-                !targetInstance.isGBufferAvailable)
+                _occlusionSource.enumValueIndex == (int)AmbientOcclusion.OcclusionSource.GBuffer)
             {
-                EditorGUILayout.HelpBox(_textNoGBuffer, MessageType.Warning);
+                if (!targetInstance.isGBufferAvailable)
+                    EditorGUILayout.HelpBox(_textNoGBuffer, MessageType.Warning);
+                else if (!_ambientOnly.hasMultipleDifferentValues && !_ambientOnly.boolValue)
+                    EditorGUILayout.HelpBox(_textGBufferNote, MessageType.Info);
             }
 
             EditorGUILayout.PropertyField(_ambientOnly);

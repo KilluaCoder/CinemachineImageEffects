2573a47204e379275cfc470e8376df52638823a8
Jens Holm <jensh@unity3d.com>
2016-06-08 11:57:44 +0200

    [fog] Initial implementation

---
+++ UnityProject/Assets/Standard Assets/Effects/CinematicEffects(ALPHA)/StylisticFog/Editor/StylisticFogEditor.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.AnimatedValues;
+
+
+namespace UnityStandardAssets.CinematicEffects
+{
+	[CanEditMultipleObjects]
+	[CustomEditor(typeof(StylisticFog))]
+    public class StylisticFogEditor : Editor
+    {
+
+		SerializedProperty _fogOpacityCurve;
+		SerializedProperty _fogColorRCurve;
+		SerializedProperty _fogColorGCurve;
+		SerializedProperty _fogColorBCurve;
+		SerializedProperty _farPlane;
+		SerializedProperty _nearPlane;
+		SerializedProperty _fogSkybox;
+		SerializedProperty _colorSelection;
+		SerializedProperty _useHeight;
+		SerializedProperty _flipHeight;
+		SerializedProperty _height;
+		SerializedProperty _baseDensity;
+        SerializedProperty _fogFactorIntensityCurve;
+		SerializedProperty _densityFalloff;
+
+		AnimBool m_UseHeight;
+
+		void OnEnable()
+		{
+			_fogOpacityCurve = serializedObject.FindProperty("settings.fogOpacityCurve");
+			_fogColorRCurve  = serializedObject.FindProperty("settings.fogColorR");
+			_fogColorGCurve  = serializedObject.FindProperty("settings.fogColorG");
+			_fogColorBCurve  = serializedObject.FindProperty("settings.fogColorB");
+			_farPlane        = serializedObject.FindProperty("settings.startDist");
+			_nearPlane       = serializedObject.FindProperty("settings.endDist");
+			_fogSkybox       = serializedObject.FindProperty("settings.fogSkybox");
+			_useHeight       = serializedObject.FindProperty("settings.useHeight");
+			_height          = serializedObject.FindProperty("settings.height");
+			_baseDensity     = serializedObject.FindProperty("settings.baseDensity");
+			_fogFactorIntensityCurve = serializedObject.FindProperty("settings.fogFactorIntensityCurve");
+            _densityFalloff = serializedObject.FindProperty("settings.densityFalloff");
+
+			m_UseHeight = new AnimBool(false);
+			m_UseHeight.valueChanged.AddListener(Repaint);
+		}
+
+		public override void OnInspectorGUI()
+        {
+			StylisticFog targetInstance = (StylisticFog)target;
+
+			serializedObject.Update();
+
+			bool propertyTextureRebake = false;
+			bool densityTextureRebake = false;
+            bool checkNearFarDistances = false;
+
+			// Curves to modify the color properties of the fog.
+			EditorGUI.BeginChangeCheck();
+			EditorGUILayout.PropertyField(_fogOpacityCurve);
+			EditorGUILayout.PropertyField(_fogColorRCurve);
+			EditorGUILayout.PropertyField(_fogColorGCurve);
+			EditorGUILayout.PropertyField(_fogColorBCurve);
+			if (EditorGUI.EndChangeCheck())
+				propertyTextureRebake = true;
+
+			// The curve that defines how much the fog cntributes according to its intensity.
+			EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(_fogFactorIntensityCurve);
+            if (EditorGUI.EndChangeCheck())
+                densityTextureRebake = true;
+
+            
+			EditorGUILayout.Space();
+
+			// Where the fog starts and where the fog reaches max saturation
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(_nearPlane);
+			EditorGUILayout.PropertyField(_farPlane);
+            if (EditorGUI.EndChangeCheck())
+                checkNearFarDistances = true;
+
+            EditorGUILayout.Space();
+
+			// Bool to decide if the skybox will be affected by fog.
+			EditorGUILayout.PropertyField(_fogSkybox);
+
+			// Parameters used when the fog volume is height based
+			EditorGUILayout.PropertyField(_useHeight);
+			m_UseHeight.target = _useHeight.boolValue;
+			if (EditorGUILayout.BeginFadeGroup(m_UseHeight.faded))
+			{
+
+                EditorGUILayout.LabelField("Height Parameters");
+
+				EditorGUI.indentLevel++;
+
+				EditorGUILayout.PropertyField(_height);
+				EditorGUILayout.PropertyField(_baseDensity);
+                EditorGUILayout.PropertyField(_densityFalloff);
+				
+				EditorGUI.indentLevel--;
+			}
+			EditorGUILayout.EndFadeGroup();
+
+			if (propertyTextureRebake)
+			{
+                targetInstance.BakeFogProperty();
+			}
+
+			if(densityTextureRebake)
+			{
+                targetInstance.BakeFogIntensity();
+			}
+
+            if (checkNearFarDistances)
+            {
+                targetInstance.correctStartEndDistances();
+            }
+            
+			serializedObject.ApplyModifiedProperties();
+		}
+    }
+}
+
+
+
---
+++ UnityProject/Assets/Standard Assets/Effects/CinematicEffects(ALPHA)/StylisticFog/Resources/StylisticFog.shader
@@ -0,0 +1,111 @@
+﻿Shader "Hidden/Image Effects/StylisticFog"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+
+	CGINCLUDE
+	#pragma shader_feature OMMIT_SKYBOX
+	#pragma shader_feature USE_HEIGHT
+	#pragma shader_feature SELECT_COLOR_BY_FOG_AMOUNT
+	#include "UnityCG.cginc"
+
+	half4 _MainTex_TexelSize;
+
+	sampler2D _MainTex;
+	sampler2D _CameraDepthTexture;
+	sampler2D _FogPropertyTexture;
+
+	// Row 0: Fog density function
+	// Row 1: Numerically integrated density function
+	sampler2D _FogHeightDensityTexture;
+
+	float4x4 _InverseViewMatrix;
+
+	uniform float _FogStartDist;
+	uniform float _FogEndDist;
+
+	uniform float _Height;
+	uniform float _BaseDensity;
+	uniform float _DensityFalloff;
+
+
+	// from https://github.com/keijiro/DepthToWorldPos
+	inline float4 DepthToWorld(float depth, float2 uv, float4x4 inverseViewMatrix)
+	{
+		float viewDepth = LinearEyeDepth(depth);
+		float2 p11_22 = float2(unity_CameraProjection._11, unity_CameraProjection._22);
+		float3 vpos = float3((uv * 2 - 1) / p11_22, -1) * viewDepth;
+		float4 wpos = mul(inverseViewMatrix, float4(vpos, 1));
+		return wpos;
+	}
+
+	inline float ComputeFogAmount(float distance)
+	{
+		float f = (distance - _FogStartDist) / (_FogEndDist - _FogStartDist);
+		return saturate(f);
+	}
+
+	half4 fragment(v2f_img i) : SV_Target
+	{
+		half4 sceneColor = tex2D(_MainTex, i.uv);
+		float depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv);
+
+#if defined(OMMIT_SKYBOX)
+		if (depth < 0.00001)
+		{
+			return sceneColor;
+		}
+#endif // defined(OMMIT_SKYBOX)
+		
+		float4 wpos = DepthToWorld(depth, i.uv, _InverseViewMatrix);
+
+		float4 fragmentToCamera = wpos - float4(_WorldSpaceCameraPos,1.);
+		float3 viewDir = normalize(fragmentToCamera);
+		float totalDistance = length(fragmentToCamera);
+
+		float effectiveDistance = max(totalDistance - _FogStartDist, 0.0);
+
+		float fogFactor = 0.;
+		float fogAmount = 0.;
+
+#if defined(USE_HEIGHT)
+
+		// Density function = d(y) = GlobalDensity * exp(-DensityFalloff * y)
+		float relativeHeight = _WorldSpaceCameraPos.y - _Height;
+
+		float falloffFactor = min(127., _DensityFalloff * relativeHeight);
+		float falloffAngle = _DensityFalloff * viewDir.y;
+
+		float heightIntegrale = _BaseDensity * exp2(-falloffFactor);
+		heightIntegrale *= (1 - exp2(-falloffAngle * effectiveDistance)) / (falloffAngle);
+
+		fogAmount = 1.0 - saturate(exp2(-heightIntegrale));
+
+#else // defined(USE_HEIGHT)
+
+		fogAmount = ComputeFogAmount(effectiveDistance);
+
+#endif // defined(USE_HEIGHT)
+
+		fogFactor = DecodeFloatRGBA(tex2D(_FogHeightDensityTexture, float2(fogAmount, 0.)));
+
+		float4 fogCol;
+		fogCol = tex2D(_FogPropertyTexture, float2(fogFactor, 0));
+		return lerp(sceneColor, half4(fogCol.xyz, 1.), fogCol.a);
+	}
+
+	ENDCG
+	SubShader
+	{
+		Pass
+		{
+			Cull Off ZWrite Off ZTest Always
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment fragment
+			ENDCG
+		}
+	}
+}

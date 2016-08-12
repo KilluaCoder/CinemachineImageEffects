b548c322cea12575062738a26df56bad6c583484
Jens Holm <jensh@unity3d.com>
2016-08-12 18:16:57 +0200

    [Fog]: Removed baking floating point textures to RGBA texture. Instead now the texture's alpha channel governs the fog intensity alone.

--- UnityProject/Assets/Standard Assets/Effects/CinematicEffects(ALPHA)/StylisticFog/StylisticFog.cs
+++ UnityProject/Assets/Standard Assets/Effects/CinematicEffects(ALPHA)/StylisticFog/StylisticFog.cs
@@ -88,9 +88,6 @@ namespace UnityStandardAssets.CinematicEffects
 			[Tooltip("Fog is fully saturated beyond this distance.")]
 			public float endDistance;
 
-			[Tooltip("How the intensity of the fog develops with the distance.")]
-			public AnimationCurve intensityDevelopment;
-
 			[Tooltip("Color selection for distance fog")]
 			public ColorSelectionType colorSelectionType;
 
@@ -102,8 +99,7 @@ namespace UnityStandardAssets.CinematicEffects
 					{
 						enabled = true,
 						fogSkybox = false,
-						endDistance = 100f,
-						intensityDevelopment = AnimationCurve.Linear(0f, 0f, 1f, 1f),
+						endDistance = 500f,
 						colorSelectionType = ColorSelectionType.Gradient,
 					};
 				}
@@ -165,6 +161,8 @@ namespace UnityStandardAssets.CinematicEffects
 		#endregion
 
 		#region fields
+		private Camera m_Camera;
+
 		[SerializeField]
 		private Texture2D m_DistanceColorTexture;
 		public Texture2D distanceColorTexture
@@ -207,27 +205,6 @@ namespace UnityStandardAssets.CinematicEffects
 			}
 		}
 
-		[SerializeField]
-		private Texture2D m_distanceFogIntensityTexture;
-		public Texture2D distanceFogIntensityTexture
-		{
-			get
-			{
-				if (m_distanceFogIntensityTexture == null)
-				{
-					m_distanceFogIntensityTexture = new Texture2D(1024, 1, TextureFormat.ARGB32, false, false)
-					{
-						name = "Fog Height density",
-						wrapMode = TextureWrapMode.Clamp,
-						filterMode = FilterMode.Bilinear,
-						anisoLevel = 0,
-					};
-					BakeFogIntensity();
-				}
-				return m_distanceFogIntensityTexture;
-			}
-		}
-
 		[SerializeField, HideInInspector]
 		private Shader m_Shader;
 		public Shader shader
@@ -259,9 +236,6 @@ namespace UnityStandardAssets.CinematicEffects
 
 		public void UpdateProperties()
 		{
-			// Update the fog density
-			BakeFogIntensity();
-
 			// Check if both color selction types are to copy
 			// If so, change one / show warning?
 			bool selectionTypeSame = distanceFog.colorSelectionType == heightFog.colorSelectionType;
@@ -278,14 +252,6 @@ namespace UnityStandardAssets.CinematicEffects
 
 		private void UpdateDistanceFogTextures(ColorSelectionType selectionType)
 		{
-			// If the gradient texture is not used, delete it.
-			if (selectionType != ColorSelectionType.Gradient || GradientIsSingleColor(distanceColorSource.gradient))
-			{
-				if (m_DistanceColorTexture != null)
-					DestroyImmediate(m_DistanceColorTexture);
-				m_DistanceColorTexture = null;
-			}
-
 			if (selectionType == ColorSelectionType.Gradient)
 			{
 				BakeFogColor(distanceColorTexture, distanceColorSource.gradient);
@@ -294,14 +260,6 @@ namespace UnityStandardAssets.CinematicEffects
 
 		private void UpdateHeightFogTextures(ColorSelectionType selectionType)
 		{
-			// If the gradient texture is not used, delete it.
-			if (selectionType != ColorSelectionType.Gradient || GradientIsSingleColor(heightColorSource.gradient))
-			{
-				if (m_HeightColorTexture != null)
-					DestroyImmediate(m_HeightColorTexture);
-				m_HeightColorTexture = null;
-			}
-
 			if (selectionType == ColorSelectionType.Gradient)
 			{
 				BakeFogColor(heightColorTexture, heightColorSource.gradient);
@@ -309,54 +267,6 @@ namespace UnityStandardAssets.CinematicEffects
 		}
 
 		#region Private Members
-		
-		// If all keys in a gradient are the same
-		// the gradient can be represented as just a single color
-		private bool GradientIsSingleColor(Gradient gradient)
-		{
-			GradientAlphaKey[] alphaKeys = gradient.alphaKeys;
-			GradientColorKey[] colorKeys = gradient.colorKeys;
-			bool allKeysAreTheSame = true;
-			float referenceAlpha = alphaKeys[0].alpha;
-			Color referenceColor = colorKeys[0].color;
-
-			foreach(GradientAlphaKey alphaKey in alphaKeys)
-			{
-				if (alphaKey.alpha != referenceAlpha)
-				{
-					allKeysAreTheSame = false;
-				}
-			}
-
-			foreach(GradientColorKey colorKey in colorKeys)
-			{
-				if (colorKey.color != referenceColor)
-				{
-					allKeysAreTheSame = false;
-				}
-			}
-
-			return allKeysAreTheSame;
-		}
-
-		private Color GradientGetSingleColor(Gradient gradient)
-		{
-			Color singleColor = gradient.colorKeys[0].color;
-			singleColor.a = gradient.alphaKeys[0].alpha;
-			return singleColor;
-		}
-
-		private void OnEnable()
-		{
-			if (!ImageEffectHelper.IsSupported(shader, true, false, this))
-				enabled = false;
-
-			GetComponent<Camera>().depthTextureMode |= DepthTextureMode.Depth;
-
-			BakeFogIntensity();
-			UpdateProperties();
-		}
-
 		private void OnDisable()
 		{
 			if (m_Material != null)
@@ -368,16 +278,12 @@ namespace UnityStandardAssets.CinematicEffects
 			if (m_HeightColorTexture != null)
 				DestroyImmediate(m_HeightColorTexture);
 
-			if (m_distanceFogIntensityTexture != null)
-				DestroyImmediate(m_distanceFogIntensityTexture);
-
 			m_Material = null;
 		}
 
 
 		private void SetDistanceFogUniforms()
 		{
-			material.SetTexture("_FogFactorIntensityTexture", distanceFogIntensityTexture);
 			material.SetFloat("_FogEndDistance", distanceFog.endDistance);
 		}
 
@@ -417,7 +323,7 @@ namespace UnityStandardAssets.CinematicEffects
 				return FogTypePass.None;
 
 			// Get the inverse view matrix for converting depth to world position.
-			Matrix4x4 inverseViewMatrix = GetComponent<Camera>().cameraToWorldMatrix;
+			Matrix4x4 inverseViewMatrix = m_Camera.cameraToWorldMatrix;
 			material.SetMatrix("_InverseViewMatrix", inverseViewMatrix);
 
 			// Decide wheter the skybox should have fog applied
@@ -437,60 +343,23 @@ namespace UnityStandardAssets.CinematicEffects
 					selectingFromDistance = false;
 				}
 
-				bool useSingleColor = (activeSelectionType == ColorSelectionType.Gradient) && GradientIsSingleColor(activeSelectionSource.gradient);
-
-				material.SetInt("_ColorSourceOneIsTexture", useSingleColor ? 0 : 1);
 				SetDistanceFogUniforms();
 				SetHeightFogUniforms();
-				if (useSingleColor)
-				{
-					material.SetInt("_ColorSourceOneIsTexture", 0);
-					Color appliedColor = GradientGetSingleColor(activeSelectionSource.gradient);
-					material.SetColor("_FogColor0", appliedColor);
-				}
+				
+				if(activeSelectionType == ColorSelectionType.Gradient)
+					material.SetTexture("_FogColorTexture0", selectingFromDistance ? distanceColorTexture : heightColorTexture);
 				else
-				{
-					material.SetInt("_ColorSourceOneIsTexture", 1);
-					if(activeSelectionType == ColorSelectionType.Gradient)
-						material.SetTexture("_FogColorTexture0", selectingFromDistance ? distanceColorTexture : heightColorTexture);
-					else
-						material.SetTexture("_FogColorTexture0", selectingFromDistance ? distanceColorSource.colorRamp : heightColorSource.colorRamp);
-				}
+					material.SetTexture("_FogColorTexture0", selectingFromDistance ? distanceColorSource.colorRamp : heightColorSource.colorRamp);
 			}
 			else
 			{
 				if (distanceFog.enabled)
-				{
-					if (distanceFog.colorSelectionType == ColorSelectionType.Gradient && GradientIsSingleColor(distanceColorSource.gradient))
-					{
-						Color appliedColor = GradientGetSingleColor(distanceColorSource.gradient);
-						material.SetColor("_FogColor0", appliedColor);
-						material.SetInt("_ColorSourceOneIsTexture", 0);
-					}
-					else
-					{
-						material.SetTexture("_FogColorTexture0", distanceFog.colorSelectionType == ColorSelectionType.Gradient ? distanceColorTexture : distanceColorSource.colorRamp);
-						material.SetInt("_ColorSourceOneIsTexture", 1);
-					}
-				}
+					material.SetTexture("_FogColorTexture0", distanceFog.colorSelectionType == ColorSelectionType.Gradient ? distanceColorTexture : distanceColorSource.colorRamp);
 
 				if (heightFog.enabled)
 				{
-					string textureSourceIdentifier = fogType == FogTypePass.HeightOnly ? "_ColorSourceOneIsTexture" : "_ColorSourceTwoIsTexture";
-
-					if (heightFog.colorSelectionType == ColorSelectionType.Gradient && GradientIsSingleColor(heightColorSource.gradient))
-					{
-						string colorPickerIdentifier = fogType == FogTypePass.HeightOnly ? "_FogColor0" : "_FogColor1";
-						Color appliedColor = GradientGetSingleColor(distanceColorSource.gradient);
-						material.SetColor(colorPickerIdentifier, appliedColor);
-						material.SetInt(textureSourceIdentifier, 0);
-					}
-					else
-					{
-						string colorTextureIdentifier = fogType == FogTypePass.HeightOnly ? "_FogColorTexture0" : "_FogColorTexture1";
-						material.SetTexture(colorTextureIdentifier, heightFog.colorSelectionType == ColorSelectionType.Gradient ? heightColorTexture : heightColorSource.colorRamp);
-						material.SetInt(textureSourceIdentifier, 1);
-					}
+					string colorTextureIdentifier = fogType == FogTypePass.HeightOnly ? "_FogColorTexture0" : "_FogColorTexture1";
+					material.SetTexture(colorTextureIdentifier, heightFog.colorSelectionType == ColorSelectionType.Gradient ? heightColorTexture : heightColorSource.colorRamp);
 				}
 			}
 
@@ -518,33 +387,6 @@ namespace UnityStandardAssets.CinematicEffects
 				Graphics.Blit(source, destination, material, (int)fogType);
 		}
 
-		public void BakeFogColor( Texture2D target,
-									 AnimationCurve colorCurveR, 
-									 AnimationCurve colorCurveG, 
-									 AnimationCurve colorCurveB, 
-									 AnimationCurve opacityCurve )
-		{
-			if (target == null)
-			{
-				return;
-			}
-
-			float fWidth = target.width;
-			Color[] pixels = new Color[target.width];
-
-			for (float i = 0f; i <= 1f; i += 1f / fWidth)
-			{
-				float r = Mathf.Clamp(colorCurveR.Evaluate(i), 0f, 1f);
-				float g = Mathf.Clamp(colorCurveG.Evaluate(i), 0f, 1f);
-				float b = Mathf.Clamp(colorCurveB.Evaluate(i), 0f, 1f);
-				float a = Mathf.Clamp(opacityCurve.Evaluate(i), 0f, 1f);
-				pixels[(int)Mathf.Floor(i * (fWidth -1f))] = new Color(r, g, b, a);
-			}
-
-			target.SetPixels(pixels);
-			target.Apply();
-		}
-
 		public void BakeFogColor(Texture2D target, Gradient gradient)
 		{
 			if (target == null)
@@ -563,80 +405,6 @@ namespace UnityStandardAssets.CinematicEffects
 
 			target.SetPixels(pixels);
 			target.Apply();
-		}
-
-
-		public void BakeFogColor(Texture2D target, Texture2D source)
-		{
-			if (target == null || source == null)
-			{
-				return;
-			}
-
-			float fWidthSource = source.width;
-			float fWidthTarget = target.width;
-			Color[] pixels = new Color[target.width];
-
-			for (float i = 0f; i <= 1f; i += 1f / fWidthTarget)
-			{
-				float targetLookup = i * (fWidthSource / fWidthTarget);
-				float lowerIndex = Mathf.Floor(targetLookup);
-				float upperIndex = Mathf.Ceil(targetLookup);
-				float lerpFactor = targetLookup - lowerIndex;
-
-				Color lower = source.GetPixel((int)lowerIndex, 0);
-				Color upper = source.GetPixel((int)upperIndex, 0);
-				Color interpolated = Color.Lerp(lower, upper, lerpFactor);
-
-				pixels[(int)Mathf.Floor(i * (fWidthTarget - 1f))] = interpolated;
-			}
-
-			target.SetPixels(pixels);
-			target.Apply();
-		}
-
-
-		public void BakeFogIntensity()
-		{
-			if (distanceFogIntensityTexture == null)
-			{
-				return;
-			}
-
-			Color[] pixels = new Color[1024];
-
-			for (float i = 0f; i <= 1f; i += 1f / 1024f)
-			{
-				int index = (int)Mathf.Floor(i * 1023f);
-				float density = Mathf.Clamp(distanceFog.intensityDevelopment.Evaluate(i), 0f, 1f);
-				density = (density == 1f) ? 0.9999f : density;
-				pixels[index] = EncodeFloatAsColor(density);
-			}
-
-			m_distanceFogIntensityTexture.SetPixels(pixels);
-			m_distanceFogIntensityTexture.Apply();
-		}
-
-		// From http://aras-p.info/blog/2009/07/30/encoding-floats-to-rgba-the-final/
-		private static Color EncodeFloatAsColor(float f)
-		{
-			Color encoded = new Color(1.0f, 255.0f, 65025.0f, 160581375.0f) * f;
-			encoded.r = encoded.r - Mathf.Floor(encoded.r);
-			encoded.g = encoded.g - Mathf.Floor(encoded.g);
-			encoded.b = encoded.b - Mathf.Floor(encoded.b);
-			encoded.a = encoded.a - Mathf.Floor(encoded.a);
-			Color temp = new Color(encoded.g, encoded.b, encoded.a, encoded.a);
-			temp *= new Color(1.0f / 255.0f, 1.0f / 255.0f, 1.0f / 255.0f, 0.0f);
-			encoded -= temp;
-			return encoded;
-		}
-
-		// From http://aras-p.info/blog/2009/07/30/encoding-floats-to-rgba-the-final/
-		private static float DecodeFloatFromColor(Color col)
-		{
-			Vector4 colorAsVec = new Vector4(col.r, col.g, col.b, col.a);
-			Vector4 temp = new Vector4(1.0f, 1.0f / 255.0f, 1.0f / 65025.0f, 1.0f / 160581375.0f);
-			return Vector4.Dot(colorAsVec, temp);
 		}
 		#endregion
 
